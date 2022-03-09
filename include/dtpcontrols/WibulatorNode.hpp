6688e6e821144ba6b3cba6a0e519f1e9a6afd9fb
Joeal Subash <joeal.subash@cern.ch>
2022-03-09 11:38:40 +0000

    Wibulator functions added from min_set_dtp2.10

--- include/dtpcontrols/WibulatorNode.hpp
+++ include/dtpcontrols/WibulatorNode.hpp
@@ -1,9 +1,11 @@
 /**
  * @file WibulatorNode.hpp
  *
- * WibulatorNode is a xample of uhal::Node derived class.
+ * WibulatorNode is a class derived from uhal::Node.
+ * Its purpose is to control the wibulator component inside
+ * DTP-firmware which is part of DUNE DAQ.
  *
- * This is part of the DUNE DAQ Software Suite, copyright 2020.
+ * This is part of the DUNE DAQ Software Suite, copyright 2022.
  * Licensing/copyright details are in the COPYING file that you should have
  * received with this code.
  */
@@ -13,27 +15,34 @@
 
 // dtpcontrols Headers
 
-// uHal Headers
+// uhal headers
+
 #include "uhal/DerivedNode.hpp"
 #include "ers/Issue.hpp"
 
 // C++ Headers
 
-
 namespace dunedaq {
   namespace dtpcontrols {
     /**
-     * @brief      Wibulator firmware node
+     * @brief      Wibulator firmware node.
      */
-    class WibulatorNode : public uhal::Node
-    {
+    class WibulatorNode : public uhal::Node {
 
       UHAL_DERIVEDNODE(WibulatorNode)
-
+      
       public:
-      explicit WibulatorNode(const uhal::Node& node);
+      explicit WibulatorNode(const uhal::Node& );
       virtual ~WibulatorNode();
-
+      void set_fire(bool);
+      void set_max_word(const uint32_t&, bool);
+      void set_buffer_address(bool);
+      void set_buffer_data(const std::vector<uint32_t>&, bool);
+      void set_chain(uint32_t, bool);  
+      uhal::ValVector<uint32_t> get_buffer_data(const uint32_t&, bool);
+      uhal::ValWord<uint32_t> get_address_width(bool);
+      uhal::ValWord<uint32_t> get_max_word(bool);
+      uhal::ValWord<uint32_t> get_size(void);      
     };
 
   } // namespace dtpcontrols
--- src/WibulatorNode.cpp
+++ src/WibulatorNode.cpp
@@ -1,34 +1,94 @@
 /**
- * @file WibulatorNode.cpp
+ * @file WibuloatorNode.cpp
  *
- * This is part of the DUNE DAQ Software Suite, copyright 2020.
+ * This is part of the DUNE DAQ Software Suite, copyright 2022.
  * Licensing/copyright details are in the COPYING file that you should have
  * received with this code.
  */
 
 #include "dtpcontrols/WibulatorNode.hpp"
-
 #include "logging/Logging.hpp"
 
-#include <map>
-#include <string>
-
 namespace dunedaq {
-namespace dtpcontrols {
+  namespace dtpcontrols {
 
-UHAL_REGISTER_DERIVED_NODE(WibulatorNode)
+    UHAL_REGISTER_DERIVED_NODE(WibulatorNode)
 
 //-----------------------------------------------------------------------------
-WibulatorNode::WibulatorNode(const uhal::Node& node)
-  : uhal::Node(node)
-{}
+    WibulatorNode::WibulatorNode(const uhal::Node& node) : uhal::Node(node) {}
 //-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
-WibulatorNode::~WibulatorNode() {}
+    WibulatorNode::~WibulatorNode(){}
 //-----------------------------------------------------------------------------
 
 //-----------------------------------------------------------------------------
+    void WibulatorNode::set_fire(bool dispatch) {
+      getNode("csr.ctrl.fire").write(0x1);
+      if(dispatch) {getClient().dispatch();}
+    }
+//-----------------------------------------------------------------------------
 
-} // namespace dtpcontrols
+//-----------------------------------------------------------------------------    
+    void WibulatorNode::set_max_word(const uint32_t& max_word, bool dispatch) {
+      getNode("csr.ctrl.max_word").write(max_word);
+      if(dispatch) {getClient().dispatch();}
+    }
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------    
+    void WibulatorNode::set_buffer_address(bool dispatch) {
+      getNode("buf.addr").write(0x0);
+      if(dispatch) {getClient().dispatch();}
+    }
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------   
+    void WibulatorNode::set_buffer_data(const std::vector<uint32_t> &values, bool dispatch) {
+      getNode("buf.data").writeBlock(values);
+      if(dispatch) {getClient().dispatch();}
+    }
+//-----------------------------------------------------------------------------    
+
+//-----------------------------------------------------------------------------    
+    void WibulatorNode::set_chain(uint32_t loop_value, bool dispatch) {
+      getNode("csr.ctrl.chain").write(loop_value);
+      if(dispatch) {getClient().dispatch();}
+    }
+//-----------------------------------------------------------------------------
+
+    
+//-----------------------------------------------------------------------------
+    uhal::ValVector<uint32_t> WibulatorNode::get_buffer_data(const uint32_t& block_size, bool dispatch) {
+      uhal::ValVector<uint32_t> buffer_data;
+      buffer_data = getNode("buf.data").readBlock(block_size);
+      if(dispatch) {getClient().dispatch();}
+      return buffer_data;
+    }
+//-----------------------------------------------------------------------------    
+
+//-----------------------------------------------------------------------------
+    uhal::ValWord<uint32_t> WibulatorNode::get_address_width(const bool dispatch) {
+      uhal::ValWord<uint32_t> addr_width{ getNode("csr.addr_width").read() };
+      if(dispatch) {getClient().dispatch();}
+      return addr_width;
+    }
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+    uhal::ValWord<uint32_t> WibulatorNode::get_max_word(const bool dispatch) {
+      uhal::ValWord<uint32_t> max_word{ getNode("csr.addr_width").read() };
+      if(dispatch) {getClient().dispatch();}
+      return max_word;
+    }
+//-----------------------------------------------------------------------------    
+
+//-----------------------------------------------------------------------------
+    uhal::ValWord<uint32_t> WibulatorNode::get_size(void) {
+      uhal::ValWord<uint32_t> size { get_address_width(true) };
+      return 1<<size;
+    }
+//-----------------------------------------------------------------------------
+    
+  } // namespace dtpcontrols
 } // namespace dunedaq
