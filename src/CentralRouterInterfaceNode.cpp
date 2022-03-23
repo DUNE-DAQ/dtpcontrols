17bce9c65fb7b54f462e21960b64abdd18ff3b4b
Joeal Subash <joeal.subash@cern.ch>
2022-03-23 13:44:41 +0000

    Central router basic features

--- src/CentralRouterInterfaceNode.cpp
+++ src/CentralRouterInterfaceNode.cpp
@@ -17,5 +17,15 @@ CentralRouterInterfaceNode::CentralRouterInterfaceNode(const uhal::Node &node) :
 
 CentralRouterInterfaceNode::~CentralRouterInterfaceNode() {}
 
+void CentralRouterInterfaceNode::enable_central_router_interface(bool dispatch) {
+  getNode("csr.ctrl.en").write(0x1);
+  if(dispatch) {getClient().dispatch();}
+}
+
+void CentralRouterInterfaceNode::drop_empty(bool dispatch) {
+  getNode("csr.ctrl.drop_empty").write(0x1);
+  if(dispatch) {getClient().dispatch();}
+}
+
 } // namespace dtpcontrols
 } // namespace dunedaq
