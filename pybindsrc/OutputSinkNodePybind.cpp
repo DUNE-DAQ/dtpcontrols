7d1f2398dcde164efe2b760e4005071e115c238a
Joeal Subash <joeal.subash@cern.ch>
2022-08-10 16:04:41 +0100

    OutputSink node cpp and pybind11 has enable, disable, clear, keep_empty, and drop_empty

--- pybindsrc/OutputSinkNodePybind.cpp
+++ pybindsrc/OutputSinkNodePybind.cpp
@@ -21,8 +21,13 @@ namespace dunedaq {
 	m.doc() = "c++ implementation of dtpcontrols Output Sink python modules"; // optional module docstring
 
 	py::class_<dtpcontrols::OutputSinkNode, uhal::Node>(m, "OutputSinkNode")
-	  .def(py::init<const uhal::Node&>());
-      }
+	  .def(py::init<const uhal::Node&>())
+	  .def("enable", &dtpcontrols::OutputSinkNode::enable, py::arg("dispatch") = true)
+	  .def("disable", &dtpcontrols::OutputSinkNode::disable, py::arg("dispatch") = true)
+	  .def("clear", &dtpcontrols::OutputSinkNode::clear, py::arg("dispatch") = true)
+	  .def("drop_idles", &dtpcontrols::OutputSinkNode::drop_idles, py::arg("dispatch") = true)
+	  .def("keep_idles", &dtpcontrols::OutputSinkNode::keep_idles, py::arg("dispatch") = true);
+	  }
 
     } // namespace python
   } // namespace timing
